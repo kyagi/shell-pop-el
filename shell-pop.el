600e0b5de8bcf8db12dd1b28f3489231493f9dbc
Tomoya Tanjo <ttanjo@gmail.com>
2013-06-15 00:48:25 +0900

    Minor document fix

--- shell-pop.el
+++ shell-pop.el
@@ -25,7 +25,7 @@
 ;;; Commentary:
 ;;
 ;; This is a utility which helps you pop up and pop out shell buffer
-;; window easily. Just do M-x shell-pop, and it is strongly recommended
+;; window easily.  Just do M-x shell-pop, and it is strongly recommended
 ;; to assign one hot-key to this function.
 ;;
 ;; I hope this is useful for you, and ENJOY YOUR HAPPY HACKING!
@@ -196,4 +196,4 @@ The input format is the same as that of `kbd'."
 
 (provide 'shell-pop)
 
-;;; shell-pop.el ends here.
+;;; shell-pop.el ends here
